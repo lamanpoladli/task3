2b5e526c06aba7dc0bb712c9a52d1b2feef22634
lamanpoladli <lamanpoladli@gmail.com>
2023-02-25 13:32:18 +0400

    alisveris

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-salam
+let customer = 1200
+if (customer < 100){
+    console.log("5%endirim");
+}
+else if (customer >= 100 && customer <= 1000){
+    console.log("8%endirim");
+}
+else {
+    console.log("10%endirim");
+}
